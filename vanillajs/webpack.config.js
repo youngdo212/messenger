61450069b9354b4edb712a35431b052f26a3cac6
mando <nsa06031@gmail.com>
2019-09-18 14:45:10 +0900

    chore: webpack dev and prod environment setup

--- vanillajs/webpack.config.js
+++ vanillajs/webpack.config.js
@@ -1,10 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
+const config = {
   entry: './src/javascripts/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: '8080',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+    ]
+  }
 };
+
+module.exports = (env, argv) => {
+  const plugins = config.plugins || [];
+
+  if (argv.mode === 'development') {
+    config.plugins = plugins.concat(new HtmlWebpackPlugin({
+      templateParameters: {
+        'sdkurl': 'http://localhost:8080/sdk/easychat.js',
+      },
+      template: './src/index.pug',
+    }))
+  }
+  if (argv.mode === 'production') {
+    config.plugins = plugins.concat(new HtmlWebpackPlugin({
+      templateParameters: {
+        'sdkurl': 'https://easychatjs.com/sdk/easychat.js',
+      },
+      template: './src/index.pug',
+    }))
+  }
+
+  return config;
+}
